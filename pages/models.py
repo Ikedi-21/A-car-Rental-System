ec2fb34e2427f6a43b0196a533c7ba2352a6d695
Ikedi-21 <ikedinachiogbonne@gmail.com>
2026-09-01 09:23:55 +0100

    last templates

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteContent(models.Model):
+    page_key = models.CharField(max_length=50, unique=True)
+    title = models.CharField(max_length=200, blank=True)
+    content = models.TextField(blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.page_key
